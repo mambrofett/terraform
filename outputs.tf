bdf069de7a0b59888c6018cb4cb66d41dcc0a5d2
mambrofett <michael.ambrosio@optiv.com>
2021-11-18 13:33:46 -0500

    added outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value	      = aws_instance.app_server.id
+output "vpc_id" {
+  description = "ID of the VPC"
+  value	      = aws_vpc.production_vpc.id
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+output "public_ip" {
+  value       = aws_instance.web_server.public_ip
+  description = "The public IP address of the web server"
 }
