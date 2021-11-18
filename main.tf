75b2703ea3585cdba4c3975a5dcebd23732d9ef7
mambrofett <michael.ambrosio@optiv.com>
2021-11-18 09:46:38 -0500

    update to main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,29 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
+resource "aws_instance" "web_server" {
+  ami                    = "ami-083654bd07b5da81d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data 	         = <<-EOF
+	      	           #!/bin/bash
+	      	           echo "Hello, World!" > index.html
+	      	           nohup busybox httpd -f -p 8080 &
+	      	           EOF
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
