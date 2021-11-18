c07c6ef2f4525815a9e23b2181f643d4a3e22e78
mambrofett <michael.ambrosio@optiv.com>
2021-11-18 16:59:19 -0500

    update

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ resource "aws_instance" "web_server" {
 	      	           EOF
   tags = {
     Name = "WebServer-${local.project_name}"
+    Terraform = "true"
+    Environment = "lab"
   }
 }
 
@@ -42,7 +44,17 @@ resource "aws_security_group" "instance" {
   }
 } 
 
-output "public_ip" {
-  value       = aws_instance.web_server.public_ip
-  description = "The public IP address of the web server"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name               = "my-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  tags		     = {
+		       Terraform = "true"
+		       Environment = "lab"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value	      = aws_vpc.production_vpc.id
-}
-
 output "public_ip" {
   value       = aws_instance.web_server.public_ip
   description = "The public IP address of the web server"
